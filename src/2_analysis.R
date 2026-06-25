87a1922e2ed3c6aaab58468bcb526900cbfc4507
danielcapistrano <39242880+danielcapistrano@users.noreply.github.com>
2026-06-25 09:12:07 +0100

    add sensitivity analysis

--- src/2_analysis.R
+++ src/2_analysis.R
@@ -6,7 +6,7 @@ library(gtsummary)
 library(plotly)
 library(marginaleffects)
 
-# Descriptive names #########################################################
+# Descriptive names ##########################################################
 
 # Table
 tab_desc <- 
@@ -67,7 +67,7 @@ dc_plot_desc(main_var = "ses_std", label_y = "Social status of a person with thi
 dc_plot_desc(main_var = "ses", label_y = "Social status of a person with this name")
 
 
-# Descriptive experiment #########################################################
+# Descriptive experiment #####################################################
 
 # Callback rates
 
@@ -144,66 +144,93 @@ dc_plot_cor("nm_signal_mean", "Signal strength of the name")
 dc_plot_cor("ladder_adj_mean", "Social status of the name")
 dc_plot_cor("ladder_adj_std", "Social status of the name")
 
-# Models #########################################################
+# Models #####################################################################
 
 vars_control <- c(
   "applicant_female", "parent", "app_citizenship", "past_unemployment",
   "fulltime_job", "childcare_success", "housing_success", "occupation"
 )
 
-m_naive  <- get_disc_model(
-  df_exp, outcome = "lm_positive", covars = c("name_mingroup", "country", vars_control), 
-  se = "cluster", cluster_var = "name_applicant"
-)
+fit_default_model <- function(covars, outcome = "lm_positive",  interaction = FALSE){
+  rhs_vars <- c(covars, vars_control)
+  get_disc_model(
+    df_exp, outcome = outcome, covars = rhs_vars, 
+    se = "cluster", cluster_var = "name_applicant", interaction = interaction
+  )
+}
 
-m_signal  <- get_disc_model(
-  df_exp, outcome = "lm_positive", covars = c("name_mingroup", "nm_signal_std", "country", vars_control), 
-  se = "cluster", cluster_var = "name_applicant"
-)
+get_output_table <- function(m1, m2, vars2){
+  tbl_merge(list(
+      tbl_regression(m1, include = "name_mingroup", conf.int = FALSE) |> bold_p(),
+      tbl_regression(m2, include = c("name_mingroup", vars2), conf.int = FALSE) |> bold_p()),
+    tab_spanner = c("Model 1", "Model 2")
+  )
+}
 
-
-tbl_merge(list(
-  tbl_regression(m_naive, include = "name_mingroup", conf.int = FALSE) |> bold_p(),
-  tbl_regression(m_signal, include = c("name_mingroup", "nm_signal_std"), conf.int = FALSE) |> bold_p()),
-  tab_spanner = c("Model 1", "Model 2"))
-
-m_interact  <- get_disc_model(
-  df_exp, outcome = "lm_positive", covars = c("name_mingroup", "nm_signal_std", "country", vars_control), 
-  se = "cluster", cluster_var = "name_applicant", interaction = TRUE
-)
-
-plot_interact <- 
-  plot_predictions(m_interact, condition = c("nm_signal_std", "name_mingroup")) +
-    labs(x="", y="Predicted values") +
+get_plot_interact <- function(model, mainvar, x_label) {
+  plot_predictions(model, condition = c(mainvar, "name_mingroup")) +
     theme_classic() +
     theme(axis.text.x = element_text(size = 12)) +
     labs(
-      x = "Signal strength of the name",
+      x = x_label,
       y = "Predicted positive callback",
       fill = "Ethnic group",
       color = "Ethnic group"
     )
+}
 
+# Model comparison
+m_naive  <- fit_default_model(covars = c("name_mingroup", "country"))
+m_signal  <- fit_default_model(covars = c("name_mingroup", "nm_signal_std", "country"))
+get_output_table(m_naive, m_signal, c("name_mingroup", "nm_signal_std"))
+
+# Interaction
+m_interact  <- fit_default_model(covars = c("name_mingroup", "nm_signal_std", "country"), interaction = TRUE)
+plot_interact <- get_plot_interact(m_interact, "nm_signal_std", "Signal strength of the name")
 ggsave(plot_interact, filename = "./output/plot_interact.png", width = 7, height = 4)
 
-# Additional analyses #############################
+# Additional analyses ########################################################
 
-m_interact_ses  <- get_disc_model(
-  df_exp, outcome = "lm_positive", covars = c("name_mingroup", "ladder_adj_std", "nm_signal_std", "country", vars_control), 
-  se = "cluster", cluster_var = "name_applicant", interaction = TRUE
-)
+# Social Status
+m_interact_ses  <- fit_default_model(covars = c("name_mingroup", "ladder_adj_mean", "nm_signal_std", "country"), interaction = TRUE)
 
 plot_interact_ses <- 
-  plot_predictions(m_interact_ses, condition = c("ladder_adj_std", "name_mingroup")) +
+  plot_predictions(m_interact_ses, condition = c("ladder_adj_mean", "name_mingroup")) +
     labs(x="", y="Predicted values") +
     theme_classic() +
     theme(axis.text.x = element_text(size = 12)) +
-    labs(
-      x = "Social status of the name",
-      y = "Predicted positive callback",
-      fill = "Ethnic group",
-      color = "Ethnic group"
-    )
+    labs(x = "Social status of the name", y = "Predicted positive callback", fill = "Ethnic group", color = "Ethnic group")
 
 ggsave(plot_interact_ses, filename = "./output/plot_interact_ses.png", width = 7, height = 4)
 
+# Invitation
+m_naive_inv  <- fit_default_model(outcome = "lm_invite", covars = c("name_mingroup", "country"))
+m_signal_inv  <- fit_default_model(outcome = "lm_invite", covars = c("name_mingroup", "nm_signal_std", "country"))
+get_output_table(m_naive_inv, m_signal_inv, c("name_mingroup", "nm_signal_std"))
+
+m_interact_inv  <- fit_default_model(outcome = "lm_invite", covars = c("name_mingroup", "nm_signal_std", "country"), interaction = TRUE)
+plot_interact_inv <- get_plot_interact(m_interact, "nm_signal_std", "Signal strength of the name")
+ggsave(plot_interact_inv, filename = "./output/plot_interact_inv.png", width = 7, height = 4)
+
+# Rejection
+m_naive_rej  <- fit_default_model(outcome = "lm_reject", covars = c("name_mingroup", "country"))
+m_signal_rej  <- fit_default_model(outcome = "lm_reject", covars = c("name_mingroup", "nm_signal_std", "country"))
+get_output_table(m_naive_rej, m_signal_rej, c("name_mingroup", "nm_signal_std"))
+
+m_interact_rej  <- fit_default_model(outcome = "lm_reject", covars = c("name_mingroup", "nm_signal_std", "country"), interaction = TRUE)
+plot_interact_rej <- get_plot_interact(m_interact_rej, "nm_signal_std", "Signal strength of the name")
+ggsave(plot_interact_rej, filename = "./output/plot_interact_rej.png", width = 7, height = 4)
+
+
+# Logistic
+m_logit_naive  <- get_disc_model(df_exp, "lm_positive", covars = c("name_mingroup", "country", vars_control), type = "logistic")
+m_logit_signal <- get_disc_model(df_exp, "lm_positive", covars = c("name_mingroup", "nm_signal_std", "country", vars_control), type = "logistic")
+get_output_table(m_logit_naive, m_logit_signal, c("name_mingroup", "nm_signal_std"))
+
+m_logit_interact <- get_disc_model(
+  df_exp, "lm_positive", covars = c("name_mingroup", "nm_signal_std", "country", vars_control), 
+  type = "logistic", interaction = TRUE
+)
+
+plot_logit_interact <- get_plot_interact(m_logit_interact, "nm_signal_std", "Signal strength of the name")
+ggsave(plot_logit_interact, filename = "./output/plot_logit_interact.png", width = 7, height = 4)
